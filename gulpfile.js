aa4910fb3472a3b99c0deea409c5cd0420f12813
Литвиненко Юрий <litvinenko-don@ukr.net>
2019-05-04 22:48:53 +0300

    изменил gulpfile, добавил refresh

--- gulpfile.js
+++ gulpfile.js
@@ -96,7 +96,7 @@ gulp.task("server", function () {
     ui: false
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css", "refresh"));
   gulp.watch("source/img/icon-*.svg", gulp.series("svg_sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
   gulp.watch("source/js/*.js", gulp.series("minify_js", "refresh"));
